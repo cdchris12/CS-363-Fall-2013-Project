d947b2a6c8c9bfad2fe6fcc435a86d65fbb2b121
cdchris12 <iamchrisbowtome@Christophers-MacBook-Pro.local>
2013-11-29 14:29:02 -0600

    Added Java Project Initial Commit
    See above

---
+++ CSci 363 Web Site/src/csci/pkg363/web/site/CSci363WebSite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csci.pkg363.web.site;
+
+/**
+ *
+ * @author iamchrisbowtome
+ */
+public class CSci363WebSite {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
