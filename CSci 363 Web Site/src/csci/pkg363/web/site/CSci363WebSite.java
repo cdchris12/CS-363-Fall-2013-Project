88c532d0b5da91377fd38b16f42bc5554027d8ab
Tyler Leben <lebents@gmail.com>
2013-11-29 16:53:01 -0600

    obj

--- CSci 363 Web Site/src/csci/pkg363/web/site/CSci363WebSite.java
+++ CSci 363 Web Site/src/csci/pkg363/web/site/CSci363WebSite.java
@@ -17,13 +17,13 @@ public class CSci363WebSite {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
       List<Student> db = new ArrayList<>();
       db.add(new Student("Johnny","Appleseed","M","johnny.appleseed", "admin",
               "336-201-9119", "123 Scotty Dr.", "Apt # 13", "Fort Pierce", "FL",
               "34952", "Darth", "Vader","902-867-5309", "409 DeathStar Lane","","Galaxy","NV","69087"));
       
-=======
+
         // TODO code application logic here
         new Login().setVisible(true);  // New Login window
         
